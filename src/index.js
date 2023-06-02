7cf0ea5377221846b7fed7533c4bcc3f8bb3afe8
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2023-06-02 20:10:38 +0300

    update

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/food-order-front1">
+    <BrowserRouter>
       <ShopContextProvider>
         <App />
       </ShopContextProvider>
